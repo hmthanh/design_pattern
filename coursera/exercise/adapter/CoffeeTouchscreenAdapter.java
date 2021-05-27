74ee90c79dce916270cd014c8d56ea8c740e237a
Thanh051205@ <hmthanhgm@gmail.com>
2021-05-27 19:44:22 +0700

    update coursera factory pattern

---
+++ coursera/exercise/adapter/CoffeeTouchscreenAdapter.java
@@ -0,0 +1,17 @@
+package coursera.exercise.adapter;
+
+public class CoffeeTouchscreenAdapter implements CoffeeMachineInterface{
+
+    @Override
+    public void chooseFirstSelection() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void chooseSecondSelection() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
